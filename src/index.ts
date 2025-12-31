853369e295589a4d6f034869db2f9207921cfd77
sanketh <sanketh.sura.1@gmail.com>
2025-12-31 19:39:39 +0530

    cors - allowed all users

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ EmailService.initialize();
 SchedulerService.start();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: true, // Allow all origins
   credentials: true
 }));
 
